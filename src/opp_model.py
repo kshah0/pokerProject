6c5e8fdb4814ba9c19bb303cce88982c68f566a1
Kush Shah <kushshah@utexas.edu>
2022-11-27 18:49:50 -0600

    added opponent model interface

---
+++ src/opp_model.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from typing import Dict, Set, List
+from card import Card
+
+from gametree.playerstate.player_id import PlayerId
+from gametree.playerstate.player_state import PlayerState
+
+from __future__ import annotations
+
+from gametree.gamestate.game_state import GameState
+from gametree.mcts.nodes.inode import INode
+
+class OpponentModel(ABC):
+
+    @abstractmethod
+    def get_action_probs(self, gs:GameState, ps:PlayerState) -> Dict[str,float]:
+        pass
+    
+
+    @abstractmethod
+    def get_bot_id() -> PlayerId:
+        pass
+
+    # May not need these 2 functions below
+    @abstractmethod
+    def set_chosen_node(self, node:INode) -> None:
+        pass
+
+    @abstractmethod
+    def get_chosen_node() -> INode:
+        pass
