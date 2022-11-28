be1be3656b3ba1be6b30f1cd5efe5bd262818abd
Kush Shah <kushshah@utexas.edu>
2022-11-28 17:33:10 -0600

    added more functions from cspoker

--- src/opp_model.py
+++ src/opp_model.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Dict
+from typing import Tuple, List
 from gametree.playerstate.player_id import PlayerId
 from gametree.playerstate.player_state import PlayerState
 
@@ -11,15 +11,22 @@ from gametree.mcts.nodes.inode import INode
 class OpponentModel(ABC):
 
     @abstractmethod
-    def get_action_probs(self, gs:GameState, ps:PlayerState) -> Dict[str,float]:
+    def get_check_bet_probabilities(self, gamestate:GameState, actor:PlayerId) -> Tuple[float,float]:
+        pass
+
+    @abstractmethod
+    def get_fold_call_raise_probabilities(self, gamestate:GameState, actor:PlayerId) -> Tuple[float, float, float]:
         pass
     
+    @abstractmethod
+    def get_showdown_probabilities(self, gamestate:GameState, actor:PlayerId) -> List[float]:
+        pass
 
     @abstractmethod
     def get_bot_id() -> PlayerId:
         pass
 
-    # May not need these 2 functions below
+    # May not need these functions below
     @abstractmethod
     def set_chosen_node(self, node:INode) -> None:
         pass
@@ -27,3 +34,24 @@ class OpponentModel(ABC):
     @abstractmethod
     def get_chosen_node() -> INode:
         pass
+
+    @abstractmethod
+    def assume_permanently(self, gamestate:GameState) -> None:
+        pass
+
+    @abstractmethod
+    def assume_temporarily(self, gamestate:GameState) -> None:
+        pass
+
+    @abstractmethod
+    def forget_last_assumption(self) -> None:
+        pass
+
+    class Factory(ABC):
+
+        @abstractmethod
+        def create(
+            self,
+            bot:PlayerId) -> OpponentModel:
+            pass
+
